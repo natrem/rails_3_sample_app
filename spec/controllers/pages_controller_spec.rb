099a7a40e14ecb5968619cd120f3fdd95fe57dea
Pascal Verdage <pascal.verdage@gmail.com>
2013-06-26 23:44:36 +0200

    end of statis pages tutorial

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,16 +3,28 @@ require 'spec_helper'
 describe PagesController do
 
   describe "GET 'home'" do
+    render_views
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    it "doit avoir le bon titre" do
+      get 'home'
+      response.should have_selector("title",
+                    :content => "Simple App | Home")
+    end
   end
 
   describe "GET 'contact'" do
+    render_views
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    it "doit avoir le bon titre" do
+      get 'contact'
+      response.should have_selector("title",
+                    :content => "Simple App | Contact")
     end
   end
 
@@ -21,6 +33,11 @@ describe PagesController do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "doit avoir le bon titre" do
+      get 'about'
+      response.should have_selector("title",
+                    :content => "Simple App | About")
     end
   end
 
